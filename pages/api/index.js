5fc179c4b0c7436f2d2b2e7b673ccb892dc5e6e4
PauloWarley <92232479+PauloWarley@users.noreply.github.com>
2022-01-30 22:32:57 -0300

    v0.0.11

--- pages/api/index.js
+++ pages/api/index.js
@@ -1015,16 +1015,20 @@ export default function handler(req, res) {
 
   try {
     console.log("1")
-    fs.readFileSync('word.txt', 'utf8')
+    correct_word = fs.readFileSync('word.txt', 'utf8')
     console.log("2")
   }
   catch{
-    console.log("3")
+      console.log("3")
       fs.writeFile('word.txt', correct_word_list[Math.floor(Math.random() * correct_word_list.length)].normalize('NFD').replace(/[\u0300-\u036f]/g, ""), function (err) {
         if (err) throw err;
         console.log('Saved!');
       });
       console.log("4")
+
+      correct_word = fs.readFileSync('word.txt', 'utf8')
+
+      console.log("4 - 1")
     
   }
 
@@ -1037,9 +1041,7 @@ export default function handler(req, res) {
 
   console.log("5")
   
-  correct_word = fs.readFileSync('word.txt', 'utf8')
-
-
+  
 
   console.log("CorrectWord", correct_word)
 
